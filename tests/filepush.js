5f3c0987f6f92d575bea1ce15c5c16f9165ed8c8
AJ ONeal <coolaj86@gmail.com>
2011-02-13 17:46:31 -0700

    organized uploader module

---
+++ tests/filepush.js
@@ -0,0 +1,13 @@
+(function () {
+  "use strict";
+
+  var filepush = require("../lib/filepush.js"),
+    fs = require('fs'),
+    path = __dirname + "/rootdir/file5.txt";
+
+  fs.stat(path, function (err, stat) {
+    stat.path = path;
+    filepush(process.argv[2] || 'www.beatgammit.com', process.argv[3] || 8022, [stat]);
+  });
+
+}());
