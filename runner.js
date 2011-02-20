1b5c33353bd0edf80a78373d825b4f225e8d801d
AJ ONeal <coolaj86@gmail.com>
2011-02-20 01:19:03 -0700

    don't attempt an upload if there isn't anything to upload

--- runner.js
+++ runner.js
@@ -2,7 +2,7 @@
   "use strict";
 
   var dirpush = require("./lib/dirpush");
-  dirpush('./tests', function () {
+  dirpush('./tests', { host: '192.168.1.101', port: 8022 }, function () {
     console.log("eagle landed hard core");
   });
 }());
