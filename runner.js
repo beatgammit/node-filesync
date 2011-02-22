e9f186add35b489400d2721f96b152dc42e53c5b
AJ ONeal <coolaj86@gmail.com>
2011-02-21 20:14:48 -0700

    improved code organization

--- runner.js
+++ runner.js
@@ -9,6 +9,10 @@
     user: 'coolaj86',
     password: 'cr4zym0nk3y'
   }, function (response) {
+    if (!response) {
+      console.log("END: The eagle never took off");
+      return;
+    }
     response.on('data', function (data) {
       console.log("DATA");
       console.log(data.toString());
