7721a7bbf51c0c75a5236aecd2d21f59bde3c2fe
AJ ONeal <coolaj86@gmail.com>
2011-02-22 21:35:24 -0700

    added fixture json dbs

---
+++ reduce-list.js
@@ -0,0 +1,13 @@
+(function () {
+  "use strict";
+
+  // full scan - moves and deletes
+  // vs
+  // quick scan - creates, moves (and half-moves), and modifies
+  var uploadDb = require('uploadDb');
+
+  uploadDb.load(function () {
+  });
+  uploadDb.load(function () {
+  });
+}();
