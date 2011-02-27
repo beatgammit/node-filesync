3d6bc2fdeaeafbc541e757230746e73c8e4db236
AJ ONeal <coolaj86@gmail.com>
2011-02-27 03:48:43 -0700

    added preliminary sqlite support, resolved previous merge conflicts, created many stub files

--- runner.js
+++ runner.js
@@ -3,8 +3,8 @@
 
   var dirpush = require("./lib/dirpush");
   dirpush('./tests', { 
-    //host: '192.168.1.101', 
-    host: 'localhost', 
+    host: '192.168.1.101', 
+    //host: 'localhost', 
     port: 8022,
     user: 'coolaj86',
     password: 'cr4zym0nk3y'
@@ -14,8 +14,7 @@
       return;
     }
     response.on('data', function (data) {
-      console.log("DATA");
-      console.log(data.toString());
+      //console.log(data.toString());
     });
     response.on('end', function () {
       console.log("END: eagle landed hard core");
