63fceac44d0ebdfc3362a7442d0bb0ce5ddd0822
Jameson Little <t.jameson.little@gmail.com>
2011-02-27 01:41:13 -0700

    Fixed some minor client bugs and added support for full mime-types

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var require;
 	"use strict";
 
 	require('noop');
+	require('long-stack-traces');
 
 	var connect = require('connect'),
         auth = require('connect-auth'),
@@ -49,34 +50,43 @@ var require;
 		onFailure(new Error("Username and password don't pass"));
 	}
 
-	function authenticateUser(req, res, next) {
-		console.log("Pre-Auth");
-		req.authenticate(['http'], function(error, authenticated) { 
-			console.log("Auth");
-			if (authenticated) {
-				return next();
-			}
-			res.writeHead(403, {'Content-Type': 'text/html'});
-			res.end("<html><h1>Bad Authentication</h1></html>\n");
-			return;
-		});
-	}
+//	function authenticateUser(req, res, next) {
+//		console.log("Pre-Auth");
+//		req.authenticate(['http'], function(error, authenticated) { 
+//			console.log("Auth");
+//			if (authenticated) {
+//				return next();
+//			}
+//			res.writeHead(403, {'Content-Type': 'text/html'});
+//			res.end("<html><h1>Bad Authentication</h1></html>\n");
+//			return;
+//		});
+//	}
 
 	function handleMeta(req, res, next){
-		var urlObj, query, dbaccess = require('./lib/dbaccess');
-		
-		urlObj = url.parse(req.url);
-		query = qs.parse(urlObj.query);
+		var urlObj, query, dbaccess = require('./lib/dbaccess'), mimeType;
 
-		if(query.contentType){
-			dbaccess.getByMimeType(query.contentType, function(err, docArray){
-				console.log(JSON.stringify(docArray));
+		switch(req.params.field){
+			case "contentType":{
+				if(req.params.value && req.params.ext){
+					mimeType = req.params.value + "/" + req.params.ext;
+					console.log("Type: " + mimeType);
+					dbaccess.getByMimeType(mimeType, function(err, docArray){
+						console.log(JSON.stringify(docArray));
+						res.writeHead(200, {'Content-Type': 'application/json'});
+						res.end(JSON.stringify(docArray));
+					});
+				}else{
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.end("{err: 'Not implemented, sorry'}");
+				}
+				break;
+			}
+			default:{
 				res.writeHead(200, {'Content-Type': 'application/json'});
-				res.end(JSON.stringify(docArray));
-			});
-		}else{
-			res.writeHead(404, {'Content-Type': 'application/json'});
-			res.end(JSON.stringify({error: "Nothing to do", query: query}));
+				res.end("{err: 'Not implemented, sorry'}");
+				break;
+			}
 		}
 	}
 
@@ -88,12 +98,13 @@ var require;
 		app.post("/file", handleUpload);
 		// doesn't work yet
 		//app.get("/file", handleDownload);
-		app.get("/meta", handleMeta);
+		app.get("/meta/:field/:value?/:ext", handleMeta);
 	}
 
 	server = connect.createServer(
+		//connect.basicAuth(validateUserPassword),
 		auth([ auth.Http({ validatePassword: validateUserPassword }) ]),
-		authenticateUser,
+		//authenticateUser,
 		form({keepExtensions: true}),
 		connect.router(routing),
 		staticProvider()
