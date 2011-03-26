5ace25b13927c024b5c7eef2477639d892da3920
Jameson Little <t.jameson.little@gmail.com>
2011-03-25 22:22:59 -0600

    JSLint and general cleanup

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ var require;
 	require('futures/forEachAsync');
 
 	var connect = require('connect'),
-        auth = require('connect-auth'),
 		db = require('./lib/dbaccess'),
         authdb = require('./users'),
 		form = require('connect-form'),
@@ -26,7 +25,7 @@ var require;
 
 	// try to rename first, copy as a backup plan
 	fs.move = function (oldPath, newPath, cb) {
-		fs.rename(oldPath, newPath, function(err){
+		fs.rename(oldPath, newPath, function (err) {
 			if (!err) {
 				return doop(cb);
 			}
@@ -35,7 +34,7 @@ var require;
 			var readStream = fs.createReadStream(oldPath),
 				writeStream = fs.createWriteStream(newPath);
 
-			util.pump(readStream, writeStream, function(err) {
+			util.pump(readStream, writeStream, function (err) {
 				if (err) {
 					return doop(cb, err);
 				}
@@ -46,64 +45,64 @@ var require;
 
 	function validateUserPassword(username, password, onSuccess, onFailure) {
 		if (authdb[username] && authdb[username] === password) {
-			if(onSuccess){
+			if (onSuccess) {
 				return onSuccess();
-			}else{
+			} else {
 				// using basicAuth
 				return true;
 			}
 		}
 
-		if(onFailure){
+		if (onFailure) {
 			onFailure(new Error("Username and password don't pass"));
 		}
 		return false;
 	}
 
-	function handleMeta(req, res, next){
+	function handleMeta(req, res, next) {
 		var dbaccess, mimeType,
 			user = req.remoteUser,
 			category = req.params.field,
 			type = req.params.value;
 
-		if(!req.params.field){
+		if (!req.params.field) {
 			return next();
 		}
 		
-		dbaccess = require('./lib/dbaccess')
+		dbaccess = require('./lib/dbaccess');
 
 		// if they have supplied the full mimetype
-		if(req.params.value){
+		if (req.params.value) {
 			mimeType = category + "/" + type;
 
-			dbaccess.getByMimeType(user, mimeType, function(error, result){
+			dbaccess.getByMimeType(user, mimeType, function (error, result) {
 				res.writeHead(200, {'Content-Type': 'application/json'});
-				if(error){
+				if (error) {
 					res.end(JSON.stringify(error));
-				}else{
+				} else {
 					res.end(JSON.stringify(result));
 				}
 			});
-		}else{
+		} else {
 			// they want a category
-			dbaccess.getMimeCategories(user, category, function(error, result){
+			dbaccess.getMimeCategories(user, category, function (error, result) {
 				var bFirst = true;
 
 				res.writeHead(200, {'Content-Type': 'application/json'});
 
-				if(error){
+				if (error) {
 					res.end(JSON.stringify(error));
 					return;
 				}
 				console.log(result);
 
 				res.write('[');
-				result.forEachAsync(function(next, item){
+				result.forEachAsync(function (next, item) {
 					console.log(item);
-					dbaccess.getByMimeType(user, item, function(error, result){
+					dbaccess.getByMimeType(user, item, function (error, result) {
 
-						result.forEach(function(tDoc){
-							if(!bFirst){
+						result.forEach(function (tDoc) {
+							if (!bFirst) {
 								res.write(',');
 							}
 							bFirst = false;
@@ -113,45 +112,45 @@ var require;
 
 						next();
 					});
-				}).then(function(){
+				}).then(function () {
 					res.end(']');
 				});
 			});
 		}
 	}
 
-	function handleRegister(req, res){
+	function handleRegister(req, res) {
 		var qData;
-		if(req.method == "GET"){
+		if (req.method === "GET") {
 			qData = qs.parse(url.parse(req.url).query);
-			db.registerUser(qData.name, qData.pass, qData.data, function(cess){
+			db.registerUser(qData.name, qData.pass, qData.data, function (cess) {
 				res.writeHead(200, {'Content-Type': 'application/json'});
 				res.end(JSON.stringify(cess));
 			});
-		}else{
-			db.registerUser(req.body.user, fields.pass, fields.data, function(err){
+		} else {
+			db.registerUser(req.body.user, req.fields.pass, req.fields.data, function (err) {
 				res.writeHead(200, {'Content-Type': 'application/json'});
-				if(err){
+				if (err) {
 					res.end(JSON.stringify({error: err}));
-				}else{
+				} else {
 					res.end(JSON.stringify({success: true}));
 				}
 			});
 		}
 	}
 
-	function handleDownload(req, res, next){
+	function handleDownload(req, res, next) {
 		var tape, gzip;
 		req.params.files = [{path: "./lib/dbaccess.js"}, {path: "./lib/import.js"}];
 		
 		gzip = new compress.GzipStream();
 		gzip.setEncoding('binary');
-		gzip.addListener('data', function(data) {
+		gzip.addListener('data', function (data) {
 			res.write(data, 'binary');
-		}).addListener('error', function(error) {
+		}).addListener('error', function (error) {
 			console.log(error);
 			throw error;
-		}).addListener('end', function() {
+		}).addListener('end', function () {
 			res.end();
 		});
 
@@ -172,64 +171,61 @@ var require;
 		});
 
 
-		if(req.params.files){
+		if (req.params.files) {
 			res.writeHead(200, {'Content-Type': 'application/x-gzip'});
 
-			req.params.files.forEachAsync(function(cb, item){
-				tape.append(item.path, function(err){
-					if(err){
+			req.params.files.forEachAsync(function (cb, item) {
+				tape.append(item.path, function (err) {
+					if (err) {
 						console.log(err);
-						throw error;
+						throw err;
 					}
 
 					cb();
 				});
-			}).then(function(){
+			}).then(function () {
 				tape.close();
 			});
 			return;
-		}else if(req.params.path || filePath){
-			if(!req.params.path){
-				req.params.path = filePath;
-			}
+		} else if (req.params.path) {
 			res.writeHead(200, {'Content-Type': 'application/x-gzip'});
 
-			tape.append(req.params.path, function(err){
-				if(err){
+			tape.append(req.params.path, function (err) {
+				if (err) {
 					console.log(err);
-					throw error;
+					throw err;
 				}
 
 				tape.close();
 			});
 			return;
-		}else{
+		} else {
 			return next();
 		}
 	}
 
-	function checkStatus(req, res, next){
+	function checkStatus(req, res, next) {
 		var bFirst = true;
 
 		res.writeHead(200, {'Content-Type': 'application/json'});
 		res.write('[');
-		req.body.files.forEachAsync(function(next, item){
-			db.fileExists(item.filestat, item.filedata, req.remoteUser, function(result){
-				if(!bFirst){
+		req.body.files.forEachAsync(function (next, item) {
+			db.fileExists(item.filestat, item.filedata, req.remoteUser, function (result) {
+				if (!bFirst) {
 					res.write(',');
 				}
 				bFirst = false;
 
-				res.write(JSON.stringify(result));;
+				res.write(JSON.stringify(result));
 				next();
 			});
-		}).then(function(){
+		}).then(function () {
 			res.end(']');
 		});
 	}
 
-	function routing(app){
-		app.post("/", function(req, res, next){
+	function routing(app) {
+		app.post("/", function (req, res, next) {
 			handleUpload(req, res, next);
 		});
 		app.post("/file", handleUpload);
