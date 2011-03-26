45cbc18a809041580549b1ac1268e0f5a4b2af5e
Jameson Little <t.jameson.little@gmail.com>
2011-03-26 00:26:05 -0600

    Moved functions for routes to separate file

--- server/server.js
+++ server/server.js
@@ -11,16 +11,12 @@ var require;
 	require('futures/forEachAsync');
 
 	var connect = require('connect'),
-		db = require('./lib/dbaccess'),
         authdb = require('./users'),
 		form = require('connect-form'),
 		fs = require('fs'),
-		url = require('url'),
-		qs = require('qs'),
 		util = require('util'),
-		handleUpload = require("./lib/import.js"),
-		compress = require('compressor'),
-		Tar = require("tar-async"),
+		handleUpload = require("./lib/import"),
+		routes = require('./lib/routes'),
 		server;
 
 	// try to rename first, copy as a backup plan
@@ -59,182 +55,19 @@ var require;
 		return false;
 	}
 
-	function handleMeta(req, res, next) {
-		var dbaccess, mimeType,
-			user = req.remoteUser,
-			category = req.params.field,
-			type = req.params.value;
-
-		if (!req.params.field) {
-			return next();
-		}
-		
-		dbaccess = require('./lib/dbaccess');
-
-		// if they have supplied the full mimetype
-		if (req.params.value) {
-			mimeType = category + "/" + type;
-
-			dbaccess.getByMimeType(user, mimeType, function (error, result) {
-				res.writeHead(200, {'Content-Type': 'application/json'});
-				if (error) {
-					res.end(JSON.stringify(error));
-				} else {
-					res.end(JSON.stringify(result));
-				}
-			});
-		} else {
-			// they want a category
-			dbaccess.getMimeCategories(user, category, function (error, result) {
-				var bFirst = true;
-
-				res.writeHead(200, {'Content-Type': 'application/json'});
-
-				if (error) {
-					res.end(JSON.stringify(error));
-					return;
-				}
-				console.log(result);
-
-				res.write('[');
-				result.forEachAsync(function (next, item) {
-					console.log(item);
-					dbaccess.getByMimeType(user, item, function (error, result) {
-
-						result.forEach(function (tDoc) {
-							if (!bFirst) {
-								res.write(',');
-							}
-							bFirst = false;
-
-							res.write(JSON.stringify(tDoc));
-						});
-
-						next();
-					});
-				}).then(function () {
-					res.end(']');
-				});
-			});
-		}
-	}
-
-	function handleRegister(req, res) {
-		var qData;
-		if (req.method === "GET") {
-			qData = qs.parse(url.parse(req.url).query);
-			db.registerUser(qData.name, qData.pass, qData.data, function (cess) {
-				res.writeHead(200, {'Content-Type': 'application/json'});
-				res.end(JSON.stringify(cess));
-			});
-		} else {
-			db.registerUser(req.body.user, req.fields.pass, req.fields.data, function (err) {
-				res.writeHead(200, {'Content-Type': 'application/json'});
-				if (err) {
-					res.end(JSON.stringify({error: err}));
-				} else {
-					res.end(JSON.stringify({success: true}));
-				}
-			});
-		}
-	}
-
-	function handleDownload(req, res, next) {
-		var tape, gzip;
-		req.params.files = [{path: "./lib/dbaccess.js"}, {path: "./lib/import.js"}];
-		
-		gzip = new compress.GzipStream();
-		gzip.setEncoding('binary');
-		gzip.addListener('data', function (data) {
-			res.write(data, 'binary');
-		}).addListener('error', function (error) {
-			console.log(error);
-			throw error;
-		}).addListener('end', function () {
-			res.end();
-		});
-
-		tape = new Tar({
-			consolidate: true,
-			output: gzip
-		});
-
-		tape.on('data', function (data) {
-			gzip.write(data);
-		});
-		tape.on('error', function (error) {
-			console.log(error);
-			throw error;
-		});
-		tape.on('end', function () {
-			gzip.close();
-		});
-
-
-		if (req.params.files) {
-			res.writeHead(200, {'Content-Type': 'application/x-gzip'});
-
-			req.params.files.forEachAsync(function (cb, item) {
-				tape.append(item.path, function (err) {
-					if (err) {
-						console.log(err);
-						throw err;
-					}
-
-					cb();
-				});
-			}).then(function () {
-				tape.close();
-			});
-			return;
-		} else if (req.params.path) {
-			res.writeHead(200, {'Content-Type': 'application/x-gzip'});
-
-			tape.append(req.params.path, function (err) {
-				if (err) {
-					console.log(err);
-					throw err;
-				}
-
-				tape.close();
-			});
-			return;
-		} else {
-			return next();
-		}
-	}
-
-	function checkStatus(req, res, next) {
-		var bFirst = true;
-
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.write('[');
-		req.body.files.forEachAsync(function (next, item) {
-			db.fileExists(item.filestat, item.filedata, req.remoteUser, function (result) {
-				if (!bFirst) {
-					res.write(',');
-				}
-				bFirst = false;
-
-				res.write(JSON.stringify(result));
-				next();
-			});
-		}).then(function () {
-			res.end(']');
-		});
-	}
-
 	function routing(app) {
-		app.post("/", function (req, res, next) {
-			handleUpload(req, res, next);
-		});
+		// default to upload, may change in the future
+		app.post("/", handleUpload);
 		app.post("/file", handleUpload);
-		app.post("/check", checkStatus);
-		// doesn't work yet
-		app.get("/file", handleDownload);
-		app.get("/meta/:field/:value?", handleMeta);
-		app.post("/register", handleRegister);
-		app.get("/register", handleRegister);
+		app.post("/check", routes.check);
+
+		// kinda works
+		app.get("/file", routes.download);
+		app.get("/meta/:field/:value?", routes.meta);
+
+		// I don't care whether it's a post or a get request
+		app.post("/register", routes.register);
+		app.get("/register", routes.register);
 	}
 
 	server = connect(
