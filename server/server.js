9b436d769cf4b0c48bf4e6ab07131dc3b165c2e6
Jameson Little <t.jameson.little@gmail.com>
2011-03-31 00:17:12 -0600

    Added rudimentary admin console

--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ var require;
 		util = require('util'),
 		routes = require('./lib/routes'),
 		settings = require('./settings'),
+		os = require('os'),
+		url = require('url'),
+		qs = require('qs'),
 		server;
 
 	// try to rename first, copy as a backup plan
@@ -54,22 +57,64 @@ var require;
 		return false;
 	}
 
+	function calculateCPU(callback) {
+		var cpus = os.cpus(),
+			startTime = +new Date,
+			tSum = [];
+
+		cpus.forEach(function (item) {
+			var tCPU = {
+				'user': item.times.user,
+				'nice': item.times.nice,
+				'sys': item.times.sys,
+				'idle': item.times.idle,
+				'irq': item.times.irq
+			};
+
+			tSum.push(tCPU);
+		});
+
+		setTimeout(function () {
+			var endTime, elapsedTime;
+
+			cpus = os.cpus();
+			endTime = +new Date;
+
+			elapsedTime = endTime - startTime;
+
+			cpus.forEach(function (item, i) {
+				tSum[i].user = (item.times.user - tSum[i].user) / elapsedTime;
+				tSum[i].nice = (item.times.nice - tSum[i].nice) / elapsedTime;
+				tSum[i].sys = (item.times.sys - tSum[i].sys) / elapsedTime;
+				tSum[i].idle = (item.times.idle - tSum[i].idle) / elapsedTime;
+				tSum[i].irq = (item.times.irq - tSum[i].irq) / elapsedTime;
+			});
+
+			callback(tSum);
+		}, settings.cpu_timeout);
+	}
+
 	function routing(app) {
 		// default to upload, may change in the future
 		app.post("/", routes.post.upload);
 		app.post("/file", routes.post.upload);
 		app.post("/check", routes.post.check);
 		app.post("/settings", function (req, res, next) {
+			var restartServer = false;
+			if (req.body.port !== settings.port || req.body.doc_root !== settings.doc_root) {
+				restartServer = true;
+			}
+			
 			settings = req.body;
-			console.log(req.body);
 			fs.writeFile('settings.json', JSON.stringify(settings));
 
 			res.writeHead(200, {'Content-Type': 'application/json'});
 			res.end(JSON.stringify(settings));
 
-			server.close();
-
-			initServer();
+			if (restartServer) {
+				server.close();
+				initServer();
+			}
 		});
 
 		app.get("/admin", routes.get.admin);
@@ -77,8 +122,33 @@ var require;
 		app.get("/file", routes.get.download);
 		app.get("/meta/:field/:value?", routes.get.meta);
 		app.get("/settings", function (req, res, next) {
-			res.writeHead(200, {'Content-Type': 'application/json'});
-			res.end(JSON.stringify(settings));
+			var tReturn = {},
+				query = qs.parse(url.parse(req.url).query);
+
+			function finish () {
+				res.writeHead(200, {'Content-Type': 'application/json'});
+				res.end(JSON.stringify(tReturn));
+			};
+
+			if (query) {
+				if (query.ram) {
+					tReturn.ram = {
+						'free': os.freemem(),
+						'total': os.totalmem()
+					};
+				}
+				if (query.cpu) {
+					calculateCPU(function (tSum) {
+						tReturn.cpus = tSum;
+						finish();
+					});
+				} else {
+					finish();
+				}
+			} else {
+				tReturn = settings;
+				finish();
+			}
 		});
 
 		// I don't care whether it's a post or a get request
